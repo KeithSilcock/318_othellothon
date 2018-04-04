51c97cbfa060039d4307c8564b065a64fafacb55
KeithSilcock <silcockk@gmail.com>
2018-04-04 09:45:38 -0700

    finished piece obj

--- includes/piece.js
+++ includes/piece.js
@@ -2,8 +2,21 @@ class Piece{
     constructor(){
         this.pieceDOM=null;
 
+        // return this.renderPiece();
     }
-    spawnPiece(){
-        this.pieceDOM = $("<div>")
+    renderPiece(){
+        this.pieceDOM = $("<div>",{
+            'class':'piece',
+
+        })
+
+        return this.pieceDOM
+    }
+
+    changeColor(colorToBe){
+        this.pieceDOM.css({
+            'background-color': colorToBe,
+        });
+        return this.pieceDOM
     }
 }
