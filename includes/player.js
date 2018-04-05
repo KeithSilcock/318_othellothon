d06e0187e95b7dae582ad8b612023850dacc535b
KeithSilcock <silcockk@gmail.com>
2018-04-04 17:41:17 -0700

    consolidated player object

---
+++ includes/player.js
@@ -0,0 +1,70 @@
+class Player {
+    constructor(name, color, img, num, time, timerCallBack) {
+        this.name = name;
+        this.color = color;
+        this.img = img;
+        this.num=num;
+
+        this._score=0;
+
+        if(!time){
+            time=300; //5 mins
+        }
+        this.maxTime=time;
+        this.currentTime = this.maxTime;
+        this.timerObj=null;
+        this.timerCallback=timerCallBack;
+        // this.movesLeft= size*size;
+    }
+
+    startTimer(){
+        this.timerObj = setInterval(this.countDown.bind(this), 1000)
+    }
+
+    countDown(){
+        if(--this.currentTime > 0){
+            console.log(this.currentTime)
+        }else{
+            //time ran out
+            this.reset();
+            this.stopTimer()
+            this.timerCallback();
+        }
+    }
+
+    stopTimer(){
+        clearInterval(this.timerObj)
+    }
+
+    reset() {
+        this.score = 0;
+        this.currentTime = this.maxTime;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+    getName(name) {
+        return this.name;
+    }
+
+    setColor(color) {
+        this.color = color;
+    }
+    getColor(color) {
+        return this.color;
+    }
+
+    getPlayerNum(){
+        return this.num;
+    }
+
+    get score(){
+        return this._score;
+    }
+    set score(newScore){
+        this._score=newScore;
+    }
+
+}
+
